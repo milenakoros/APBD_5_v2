a9b70917a1ff535b8b195b6b071b8089a24f0277
s28502 <s28502@pjwstk.edu.pl>
2024-05-12 21:42:08 +0200

    Setup

---
+++ APBD_5_v2/APBD_5_v2/Program.cs
@@ -0,0 +1,31 @@
+using APBD_5_v2.Repositories;
+using APBD_5_v2.Services;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers().AddXmlSerializerFormatters();
+        builder.Services.AddScoped<IAnimalsRepository, AnimalsRepository>();
+        builder.Services.AddScoped<IAnimalsService, AnimalsService>();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.MapControllers();
+
+        app.Run();
+    }
+}
